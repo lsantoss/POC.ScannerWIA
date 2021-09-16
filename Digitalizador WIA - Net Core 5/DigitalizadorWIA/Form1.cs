4b074c4093fd8e1f1e9118df45ef08da94a8e85c
Lucas Santos <lucas.santos@otacom.com.br>
2021-09-16 19:48:00 -0300

    Criando versão para .net core 5

---
+++ Digitalizador WIA - Net Core 5/DigitalizadorWIA/Form1.cs
@@ -0,0 +1,172 @@
+﻿using DigitalizadorWIA.Enums;
+using DigitalizadorWIA.Scanners;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using WIA;
+
+namespace DigitalizadorWIA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListarScanners();
+            txtLocalDestino.Text = @"D:\";
+            cbFormato.SelectedIndex = 0;
+        }
+
+        private void BtnLocalDestino_Click(object sender, EventArgs e)
+        {
+            var folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+
+            var result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+                txtLocalDestino.Text = folderDlg.SelectedPath;
+        }
+
+        private void BtnScanner_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+            var scanner = lstScanners.SelectedItem as ScannerWIA;
+            var imagem = new ImageFile();
+            var path = string.Empty;
+
+            if (scanner == null)
+            {
+                MessageBox.Show("Selecione um scanner na lista!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (string.IsNullOrEmpty(txtNomeArquivo.Text))
+            {
+                MessageBox.Show("Digite um nome para o arquivo!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                var imagemExtensao = DefinirExtensaoArquivo(cbFormato.SelectedIndex);
+                path = Path.Combine(txtLocalDestino.Text, txtNomeArquivo.Text + imagemExtensao);
+
+                if (File.Exists(path))
+                {
+                    MessageBox.Show("Atualmente existe um arquivo com mesmo nome e formato no local de destino!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            try
+            {
+                imagem = scanner.Escanear((ETipoArquivo)cbFormato.SelectedIndex);
+                imagem.SaveFile(path);
+                pictureBox.Image = new Bitmap(path);
+                MessageBox.Show("Documento digitalizado com sucesso!", "Sucesso!");
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possível salvar o documento", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnGerarPDF_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+            var scanner = lstScanners.SelectedItem as ScannerWIA;
+            var path = $"{txtLocalDestino.Text}{txtNomeArquivo.Text}.pdf";
+
+            if (scanner == null)
+            {
+                MessageBox.Show("Selecionar um scanner na lista!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (string.IsNullOrEmpty(txtNomeArquivo.Text))
+            {
+                MessageBox.Show("Digite um nome para o arquivo!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                if (File.Exists(path))
+                {
+                    MessageBox.Show("Atualmente existe um arquivo com mesmo nome e formato no local de destino!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            try
+            {
+                var nomeImagemTemp = $"{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}.png";
+                var pathImagemTemp = Path.Combine(txtLocalDestino.Text, nomeImagemTemp);
+                var imagemTemp = scanner.Escanear(ETipoArquivo.PNG);
+                imagemTemp.SaveFile(pathImagemTemp);
+
+                using (var pdf = new Document(PageSize.LETTER, 5f, 5f, 5f, 5f))
+                {
+                    PdfWriter.GetInstance(pdf, new FileStream(path, FileMode.Create));
+                    pdf.Open();
+                    var imagemAnexadaPdf = iTextSharp.text.Image.GetInstance(pathImagemTemp);
+                    imagemAnexadaPdf.ScaleToFit(650f, 780f);
+                    imagemAnexadaPdf.Alignment = Element.ALIGN_CENTER;
+                    pdf.Add(imagemAnexadaPdf);
+                }
+
+                File.Delete(pathImagemTemp);
+                MessageBox.Show("Documento PDF criado com sucesso!", "Sucesso!");
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possível criar arquivo PDF!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion Eventos
+
+        #region Metodos Privados
+
+        private void ListarScanners()
+        {
+            lstScanners.Items.Clear();
+            var deviceManager = new DeviceManager();
+
+            foreach (DeviceInfo dispositivo in deviceManager.DeviceInfos)
+                if (dispositivo.Type == WiaDeviceType.ScannerDeviceType)
+                    lstScanners.Items.Add(new ScannerWIA(dispositivo));
+        }
+
+        private string DefinirExtensaoArquivo(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return ".png";
+
+                case 1:
+                    return ".jpeg";
+
+                case 2:
+                    return ".tiff";
+
+                case 3:
+                    return ".bmp";
+
+                case 4:
+                    return ".gif";
+                default:
+                    return ".png";
+            }
+        }
+
+        #endregion Metodos Privados
+    }
+}
---
+++ Digitalizador WIA - Net Core 5/DigitalizadorWIA/Scanners/ScannerWIA.cs
@@ -0,0 +1,120 @@
+﻿using DigitalizadorWIA.Enums;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WIA;
+
+namespace DigitalizadorWIA.Scanners
+{
+    public class ScannerWIA
+    {
+        private readonly DeviceInfo _deviceInfo;
+
+        public ScannerWIA(DeviceInfo deviceInfo)
+        {
+            _deviceInfo = deviceInfo;
+        }
+
+        public ImageFile Escanear(ETipoArquivo tipoArquivo)
+        {
+            try
+            {
+                var device = _deviceInfo.Connect(); // Conecta com o dispositivo                
+                var scanerItem = device.Items[1]; // Seleciona o scanner
+
+                ConfigurarScanner(scanerItem);
+
+                object scanResult = null;
+
+                switch (tipoArquivo)
+                {
+                    case ETipoArquivo.PNG:
+                        scanResult = scanerItem.Transfer("{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}");
+                        break;
+                    case ETipoArquivo.JPEG:
+                        scanResult = scanerItem.Transfer("{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}");
+                        break;
+                    case ETipoArquivo.TIFF:
+                        scanResult = scanerItem.Transfer("{B96B3CB1-0728-11D3-9D7B-0000F81EF32E}");
+                        break;
+                    case ETipoArquivo.BMP:
+                        scanResult = scanerItem.Transfer("{B96B3CAB-0728-11D3-9D7B-0000F81EF32E}");
+                        break;
+                    case ETipoArquivo.GIF:
+                        scanResult = scanerItem.Transfer("{B96B3CB0-0728-11D3-9D7B-0000F81EF32E}");
+                        break;
+                }
+
+                return scanResult != null ? (ImageFile)scanResult : null;
+            }
+            catch (COMException e)
+            {
+                uint errorCode = (uint)e.ErrorCode;
+
+                switch (errorCode)
+                {
+                    case 0x80210006:
+                        MessageBox.Show("A scanner não está pronta ou está ocupada!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    case 0x80210064:
+                        MessageBox.Show("O processo de digitalização foi cancelado!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    default:
+                        MessageBox.Show("Ocorreu um erro não conhecido!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+
+                return null;
+            }
+        }
+
+        private static void ConfigurarScanner(IItem scannnerItem)
+        {
+            try
+            {
+                var resolucaoDPI = 300;
+                var scannerInicioEsquerdaPixel = 0;
+                var scannerInicioTopoPixel = 0;
+                var larguraPixel = 2500;
+                var alturaPixel = 3400;
+                var iluminacaoPorcentagem = 0;
+                var contrastePercentagem = 0;
+                var colorMode = 1;
+
+                const string WIA_SCAN_COLOR_MODE = "6146";
+                const string WIA_HORIZONTAL_SCAN_RESOLUTION_DPI = "6147";
+                const string WIA_VERTICAL_SCAN_RESOLUTION_DPI = "6148";
+                const string WIA_HORIZONTAL_SCAN_START_PIXEL = "6149";
+                const string WIA_VERTICAL_SCAN_START_PIXEL = "6150";
+                const string WIA_HORIZONTAL_SCAN_SIZE_PIXELS = "6151";
+                const string WIA_VERTICAL_SCAN_SIZE_PIXELS = "6152";
+                const string WIA_SCAN_BRIGHTNESS_PERCENTS = "6154";
+                const string WIA_SCAN_CONTRAST_PERCENTS = "6155";
+
+                DefinirPropriedades(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_RESOLUTION_DPI, resolucaoDPI);
+                DefinirPropriedades(scannnerItem.Properties, WIA_VERTICAL_SCAN_RESOLUTION_DPI, resolucaoDPI);
+                DefinirPropriedades(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_START_PIXEL, scannerInicioEsquerdaPixel);
+                DefinirPropriedades(scannnerItem.Properties, WIA_VERTICAL_SCAN_START_PIXEL, scannerInicioTopoPixel);
+                DefinirPropriedades(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_SIZE_PIXELS, larguraPixel);
+                DefinirPropriedades(scannnerItem.Properties, WIA_VERTICAL_SCAN_SIZE_PIXELS, alturaPixel);
+                DefinirPropriedades(scannnerItem.Properties, WIA_SCAN_BRIGHTNESS_PERCENTS, iluminacaoPorcentagem);
+                DefinirPropriedades(scannnerItem.Properties, WIA_SCAN_CONTRAST_PERCENTS, contrastePercentagem);
+                DefinirPropriedades(scannnerItem.Properties, WIA_SCAN_COLOR_MODE, colorMode);
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao configurar scanner", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void DefinirPropriedades(IProperties propriedades, object nomePropriedade, object valorPropriedade)
+        {
+            var propriedade = propriedades.get_Item(ref nomePropriedade);
+            propriedade.set_Value(ref valorPropriedade);
+        }
+
+        public override string ToString()
+        {
+            return (string)_deviceInfo.Properties["Name"].get_Value();
+        }
+    }
+}
