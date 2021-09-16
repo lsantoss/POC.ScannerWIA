61bf4c52ce36d0275b0828d5dbdd273adf1740fe
Lucas Santos <lucas.santos@otacom.com.br>
2021-09-16 18:54:57 -0300

    Recriando repositório

---
+++ Digitalizador WIA - Net Framework 4.8/DigitalizadorWIA/Enums/ETipoArquivo.cs
@@ -0,0 +1,12 @@
+﻿namespace DigitalizadorWIA.Enums
+{
+    public enum ETipoArquivo
+    {
+        PNG = 0,
+        JPEG = 1,
+        TIFF = 2,
+        BMP = 3,
+        GIF = 4,
+        PDF = 999
+    }
+}
